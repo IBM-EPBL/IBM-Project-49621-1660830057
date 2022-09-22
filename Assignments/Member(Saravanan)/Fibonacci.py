e4d5080d3f6fa8a5ae2bb2a89de46ba270d3387c
CoderSaravanan <68699634+CoderSaravanan@users.noreply.github.com>
2022-09-22 19:56:40 +0530

    Add files via upload

---
+++ Assignments/Member(Saravanan)/Fibonacci.py
@@ -0,0 +1,14 @@
+a = 0
+b = 1
+
+n = int(input("Enter the range of fibonacci numbers you wish to find"))
+
+print(a)
+print(b)
+
+for i in range(0,n-2):
+    fib = a + b
+    print(fib)
+    a = b
+    b = fib
+    i = i + 1
