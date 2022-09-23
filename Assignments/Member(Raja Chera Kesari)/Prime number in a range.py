4bf110e0f47ba41ef558a0522937f962c304fa6d
YoganRaja <113256693+YoganRaja@users.noreply.github.com>
2022-09-23 13:58:39 +0530

    Create Prime number in a range.py

---
+++ Assignments/Member(Raja Chera Kesari)/Prime number in a range.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the lower bound: "))
+b = int(input("Enter the upper bound: "))
+
+for i in range(a,b+1):
+  if i > 1:
+    for j in range(2, i):
+      if (i % j) == 0:
+        break
+    else:
+      print(i , " is a prime number")
+  else:
+      print(i , " is neither prime nor composite")
